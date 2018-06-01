eb302c27c3d0c730270b05c16f801c8e7858b4be
JuanM11 <juanpmoncayo@umariana.edu.co>
2018-05-31 21:20:36 -0500

    Primer Commit

---
+++ src/main/java/com/example/bolaospc/maltrato_0/MapsActivity.java
@@ -0,0 +1,154 @@
+package com.example.bolaospc.maltrato_0;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    public Marker marcador;
+    public double lat = 0.0;
+    public double lon = 0.0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_mapa_lug);
+        int status= GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+        if(status== ConnectionResult.SUCCESS)
+        {
+            SupportMapFragment mapFragment=(SupportMapFragment)getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+
+        }else{
+            Dialog dialog=GooglePlayServicesUtil.getErrorDialog(status,(Activity)getApplicationContext(),10);
+            dialog.show();
+        }
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        Bundle bundle = getIntent().getExtras();
+        int lugar = bundle.getInt("");
+
+        Bundle parametros = this.getIntent().getExtras();
+        String datos = parametros.getString("");
+
+        if(datos.equalsIgnoreCase(""))
+        {
+            LatLng sydney = new LatLng(1.2095222,-77.2791655);
+            //mMap.addMarker(new MarkerOptions().position(sydney).title("").snippet(""+"").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+            marcador = googleMap.addMarker(
+                    new MarkerOptions()
+
+                            .title("")
+            );
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15));
+        }
+
+/*        switch (lugar) {
+            case 1:
+
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                UiSettings uiSettings = mMap.getUiSettings();
+                uiSettings.setZoomControlsEnabled(true);
+                // Add a marker in Sydney and move the camera
+                LatLng sydney = new LatLng(-34, 151);
+                mMap.addMarker(new MarkerOptions().position(sydney).title("").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+                float zoomlevel = 16;
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, zoomlevel));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ub();
+                    return;
+                }
+                mMap.setMyLocationEnabled(true);
+        }*/
+    }
+    public void agregarmark(double lat, double lon) {
+        LatLng coor = new LatLng(lat, lon);
+        CameraUpdate miubicacion = CameraUpdateFactory.newLatLngZoom(coor, 16);
+        if (marcador != null) marcador.remove();
+        {
+            marcador = mMap.addMarker(new MarkerOptions().position(coor).title("tu estas aqui"));
+            mMap.animateCamera(miubicacion);
+        }
+    }
+
+    public void actualizarubicacion(Location location) {
+        if (location != null) {
+            lat = location.getLatitude();
+            lon = location.getLongitude();
+            agregarmark(lat, lon);
+
+        }
+    }
+    LocationListener locListe = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            actualizarubicacion(location);
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+    private void ub() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationManager locma = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location loca = locma.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        actualizarubicacion(loca);
+        locma.requestLocationUpdates(LocationManager.GPS_PROVIDER,15000,0,locListe);
+    }
+}
---
+++ src/main/java/com/example/bolaospc/maltrato_0/adapters/ParejaAdapter.java
@@ -0,0 +1,84 @@
+package com.example.bolaospc.maltrato_0.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import com.example.bolaospc.maltrato_0.R;
+import com.example.bolaospc.maltrato_0.models.Ninios;
+import com.example.bolaospc.maltrato_0.models.Pareja;
+
+import java.util.ArrayList;
+
+public class ParejaAdapter extends RecyclerView.Adapter<ParejaAdapter.ViewHolder> {
+
+
+    private ArrayList<Pareja> datos;
+    private Context context;
+
+    public ParejaAdapter(Context context) {
+        this.context = context;
+        datos = new ArrayList<>();
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardparejas, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        Pareja p = datos.get(position);
+        holder.TV1.setText(p.getHombre());
+        holder.TV2.setText(p.getHombreCasos());
+        holder.TV3.setText(p.getMujer());
+        holder.TV4.setText(p.getMujerCasos());
+        holder.TV5.setText(p.getTotal());
+        holder.TV6.setText(p.getTotalCasos());
+
+
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return datos.size();
+    }
+
+
+    public void adicionarLista(ArrayList<Pareja> lista) {
+        datos.addAll(lista);
+        notifyDataSetChanged();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView TV1, TV2, TV3,TV4,TV5,TV6;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            TV1 = (TextView) itemView.findViewById(R.id.txtNHombres);
+            TV2 = (TextView) itemView.findViewById(R.id.txtPHombres);
+            TV3 = (TextView) itemView.findViewById(R.id.txtNMujeres);
+            TV4 = (TextView) itemView.findViewById(R.id.txtPMujeres);
+            TV5 = (TextView) itemView.findViewById(R.id.txtTotal);
+            TV6 = (TextView) itemView.findViewById(R.id.txtPTotal);
+
+
+        }
+    }
+}
